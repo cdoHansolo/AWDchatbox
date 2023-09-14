bc34b987d9d3bed85aa8c733d94a73aaa63669b8
cdoHansolo <86644072+cdoHansolo@users.noreply.github.com>
2023-09-14 18:48:59 +0800

    Delete socialnet directory

--- socialnet/friendlist/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user','date_of_birth','gender') #display fields
-    list_filter = ('gender') #filtering gender
-    search_fields = ('user__username', 'date_of_birth', 'gender__gender') #search the contents
- 
-admin.site.register(UserProfile, UserProfileAdmin)
--- socialnet/friendlist/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FriendlistConfig(AppConfig):
-    name = 'friendlist'
